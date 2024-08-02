2bf42ea4b6658c98fb969f95cf0941319687412f
Kyle Taborski <ktabors@yahoo.com>
2024-08-02 16:40:01 -0700

    removing commented out code

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
 'use client'
 
 import styles from '../styles/Home.module.css';
-//import { ColorPicker } from '@react-spectrum/color';
-// import { ColorEditor, ColorPicker } from '@react-spectrum/color';
-// import { Provider } from '@react-spectrum/provider';
-//import { theme as defaultTheme } from '@react-spectrum/theme-default';
-//import { Button } from '@react/experimental-s2';
-// import '@react/experimental-s2/page.css';
-//import styles from '../styles/layer.css';
 
 export default function Home() {
   return (
